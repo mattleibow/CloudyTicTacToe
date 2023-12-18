065eff9d5345f436d2e0d25022cf161dc1d73bef
Matthew Leibowitz <mattleibow@live.com>
2023-12-18 23:32:00 +0200

    Add the initial code from the template

---
+++ app/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CloudyTicTacToe;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ app/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CloudyTicTacToe;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
