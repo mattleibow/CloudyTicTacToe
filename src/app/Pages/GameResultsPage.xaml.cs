2917564505f54ed88d792735e6087eee35035b43
Matthew Leibowitz <mattleibow@live.com>
2023-12-30 10:27:14 +0200

    Add initial screens and basic navigation (#32)
    The screens do nothing, but exist. Navigation is "there", but not complete.
    
    Fixes #23

---
+++ src/app/Pages/GameResultsPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace CloudyTicTacToe;
+
+public partial class GameResultsPage : Popup
+{
+	public GameResultsPage()
+	{
+		InitializeComponent();
+	}
+
+	public string? State { get; set; }
+
+	private async void OnNewGameClicked(object sender, EventArgs e)
+	{
+		await CloseAsync();
+		await Shell.Current.GoToAsync("..?newgame=true");
+	}
+
+	private async void OnHomeClicked(object sender, EventArgs e)
+	{
+		await CloseAsync();
+		await Shell.Current.GoToAsync("//home");
+	}
+}
+
